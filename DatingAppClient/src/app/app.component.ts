25c7fe8d0bfec3df524be3ecd0f798c212da3da7
Ahtasham-ul-haq-90 <Ahtashamulhaq198@gmail.com>
2025-08-01 00:38:18 +0500

    walking skeleton

--- DatingAppClient/src/app/app.component.ts
+++ DatingAppClient/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'DatingAppClient';
+  Users : any;
+  constructor(private http : HttpClient){};
+ngOnInit() {
+  this.http.get('https://localhost:7053/api/User').subscribe(response=> {
+    this.Users = response
+  }, error =>{
+    console.error(error);
+  },
+);
+}
 }
